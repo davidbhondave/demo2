41922fbf1a8e546ee2f89348412a0c993d5c0cd0
davidbhondave <91135934+daviddhondave@users.noreply.github.com>
2021-09-28 16:47:09 +0530

    array1

---
+++ ArrayPract.java
@@ -0,0 +1,98 @@
+import java.util.Scanner;
+
+public class ArrayPract {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner (System.in);
+		/*
+		int arr[]=new int[5];
+		System.out.println("enter the array elements:");
+		for(int i=0;i<5;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		System.out.println("array elements are:");
+		int sum=0;
+		for(int i=0;i<5;i++)
+		{
+			System.out.print(arr[i]+" ");
+			sum=sum+arr[i];
+		}
+		System.out.println("sum:"+sum);
+		*/
+		
+		/*
+		//linear Search Algorithm..
+		int arr[]=new int[7];
+		System.out.println("enter the array ele:");
+		for(int i=0;i<7;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		System.out.println("array ele are:");
+		for(int i=0;i<7;i++)
+		{
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println();
+		
+		int num;
+		boolean flag=false;
+		System.out.println("enter the ele. needs to be searched:");
+		num=sc.nextInt();
+		for(int i=0;i<7;i++)
+		{
+			if(num==arr[i])
+			{
+				flag=true;
+				break;
+			}
+			else
+				flag=false;
+		}
+		if(flag==true)
+		{
+			System.out.println("number is found");
+		}
+		else
+			System.out.println("number is not found");
+			*/
+		
+		/*
+		int arr[]=new int[5];
+		System.out.println("enter the array ele:");
+		for(int i=0;i<5;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		System.out.println("array ele are:");
+		for(int i=0;i<5;i++)
+		{
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println();
+		int even=0;
+		int odd=0;
+		for(int i=0;i<5;i++)
+		{
+			if(arr[i]%2==0)
+			{
+				System.out.print("even no: "+arr[i]+" ");
+				++even;
+			}
+			else if(arr[i]%2!=0) 
+			{
+				System.out.print("odd no: "+arr[i]+" ");
+				++odd;
+			}
+		
+		}
+		System.out.println();
+		System.out.println("total even:"+even +"  total odd:"+odd);
+		*/
+		//Q In above question copy even digits from above array to ar_even array
+		//and odd digits to ar_odd array
+		
+	}
+
+}
